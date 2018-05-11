9c21381ae98c344804cd946d010e8516b9c81578
mwx2006 <37146632+mwx2006@users.noreply.github.com>
2018-05-11 11:56:29 +0800

    Add files via upload

--- bilibili下载.py
+++ bilibili下载.py
@@ -1,9 +1,9 @@
-import easygui
+﻿import easygui
 import os
 ynbox=easygui.ynbox('选择下载方式','bilibili下载',('AV号下载','链接下载'))
 if ynbox == True:
     b=easygui.enterbox(msg='输入AV号', title='bilibili下载', strip=True, image=None, root=None)
-    c='you-get -d -l -o /home/mwx/视频/bilibili https://www.bilibili.com/video/'+b
+    c='python -m you_get -d -l -o /home/mwx/视频/bilibili https://www.bilibili.com/video/'+b
     print(c)
     os.system(c)
 elif ynbox == False:
@@ -13,7 +13,6 @@ elif ynbox == False:
         e=d[0:d.rfind('?', 1)]
     elif g < 50 :
         e=d
-    f='you-get -d -l -o /home/mwx/视频/bilibili '+e
+    f='python -m you_get -d -l -o E:\lenovo\py '+e
     print(f)
     os.system(f)
-# + 1
